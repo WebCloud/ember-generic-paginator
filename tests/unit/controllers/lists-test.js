e992567ac931baa1f9820b5b4d97d95bf06cc20c
Chris Watts <cwatts@simplymeasured.com>
2015-01-28 11:05:43 -0800

    Upgrading ember cli and ember

--- tests/unit/controllers/lists-test.js
+++ tests/unit/controllers/lists-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('controller:lists-controller', 'ListsControllerController', {
+moduleFor('controller:lists', 'ListsController', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
